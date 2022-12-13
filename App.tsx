52340cbee9fbdf6c40e77b4caef6bdc51ffa2d87
Thomas Coldwell <31568400+thomas-coldwell@users.noreply.github.com>
2022-12-13 08:10:22 +0000

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { Asset } from "expo-asset";
+import * as FileSystem from "expo-file-system";
+
+const testAsset = Asset.fromModule(require("./assets/icon.png"));
+const serverUrl = "http://127.0.0.1:3000";
+
+const uploadFile = async () => {
+  const localAssetUri = (await testAsset.downloadAsync()).localUri;
+  if (localAssetUri == null) return;
+  await FileSystem.uploadAsync(`${serverUrl}/binaryUpload`, localAssetUri, {
+    headers: {
+      Authorization: "an auth token",
+    },
+    httpMethod: "POST",
+    uploadType: FileSystem.FileSystemUploadType.BINARY_CONTENT,
+    sessionType: FileSystem.FileSystemSessionType.BACKGROUND,
+  });
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Upload file" onPress={uploadFile} />
     </View>
   );
 }
@@ -13,8 +30,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
